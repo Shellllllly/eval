b71285f0142695877b1a5cf95f41f4292beafa0d
Shellllllly <1377647235@qq.com>
2020-07-28 10:40:16 +0800

    first commit

---
+++ thread.py
@@ -0,0 +1,36 @@
+import threading
+
+import job
+import logging
+import grpc
+import time
+ 
+from atom.interceptors import header_manipulator_client_interceptor
+from atom import meta_pb2
+from atom import training_pb2
+from atom import job_pb2
+from atom import job_pb2_grpc
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.DEBUG)
+ 
+logging.basicConfig()
+
+
+
+class evalThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+
+        endpoint = 'apiserver.atom.sl.supremind.info:443' # apiserver 地址，不同集群地址不一样
+        creator = 'xxxxxx' # 创建者，一般为姓名拼音
+        private_token = 'xxxxxxxx'
+        job.eval(endpoint, creator, 'testModel', private_token)
+
+        print("Exiting " + self.name)
+
